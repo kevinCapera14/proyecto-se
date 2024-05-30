261aea119fe7e09a519238c37d44a05afb28e054
kevinCapera14 <kevincapera0105@gmail.com>
2024-05-30 10:04:37 -0500

    Se agrego el Poryecto

---
+++ src/constants/deparamentos.js
@@ -0,0 +1,47 @@
+export const departamentos = [
+    {
+        titulo: "Cesar",
+        descricion: "",
+        preguntas: [
+            {
+                numb: 1,
+                question: "¿Cuál es la capital del Cesar?",
+                answer: "Valledupar",
+                options: [
+                    "Valledupar",
+                    "Sincelejo",
+                    "Aguachica",
+                    "Gamarra"
+                ]
+            },
+            {
+                numb: 2,
+                question: "¿Cuál es la capital del Cesar?",
+                answer: "Valledupar",
+                options: [
+                    "Valledupar",
+                    "Sincelejo",
+                    "Aguachica",
+                    "Gamarra"
+                ]
+            }
+        ]
+    },
+    {
+        titulo: "Antioquia",
+        descricion: "",
+        preguntas: [
+            {
+                numb: 1,
+                question: "¿Cuál es la capital de Antioquia?",
+                answer: "Medellín",
+                options: [
+                    "Valledupar",
+                    "Sincelejo",
+                    "Medellín",
+                    "Gamarra"
+                ]
+            }
+        ]
+    }
+]
---
+++ src/types/api.ts
@@ -0,0 +1,12 @@
+export const departmentsData= {
+    nariño:{
+        id:'CONAR',
+        name: 'Narino',
+        content: 'Nariño es uno de los treinta y dos departamentos que forman la República de Colombia. Su capital es Pasto.',
+    },
+    putumayo:{
+        id:'COPUT',
+        name: 'Putumayo',
+        content: 'Putumayu) es uno de los treinta y dos departamentos que forman la República de Colombia.',
+    }
+};
